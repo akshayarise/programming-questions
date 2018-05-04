1c678a33bad18e5e4d59dd8d62face95f24f8176
akshayarise <akshayawakearise@gmail.com>
2018-05-04 22:52:05 +0530

    some files are added

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -7,9 +7,31 @@ typedef struct node{
  }Node;
  Node *head,*temp,*hold;
 
+Node * insertAtBeg(int num,Node *head){
+  Node *t;
+  t = (Node *)malloc(sizeof(Node));
+  t->data = num;
+  t->next = head;
+  head = t; 
+  return head;
+}
+
+Node * insertAtEnd(int num,Node *head){
+  Node *last ,*t;
+  last = (Node *)malloc(sizeof(Node));
+  t = head; 
+  while(t->next){
+   t = t->next;
+  }
+  t->next = last;
+  last->data = num;
+  last->next = NULL;
+  return head;
+}
+
 int main(){
    
-   int n,data;
+   int n,data,beginData,lastData;
    printf("enter the number of nodes\n");
    scanf("%d",&n);
    if(n<1){
@@ -38,6 +60,26 @@ int main(){
    while(temp){
    	printf("%d\n",temp->data);
    	temp = temp->next;
+   }
+   printf("enter the number to be inserted at the beginning\n");
+   scanf("%d",&beginData);
+   Node *newhead = insertAtBeg(beginData,head);
+  // traversing the singly linked list after inserting the element at the beginning.
+   Node *newtemp = newhead;
+   while(newtemp){
+    printf("\n%d\n",newtemp->data);
+    newtemp = newtemp->next;
+   }
+
+   printf("enter the number to be inserted at the last of the linked list\n");
+   scanf("%d",&lastData);
+   Node *head3 = insertAtEnd(lastData,head);
+
+// traversing the singly linked list after inserting the element at the end.
+   Node *temp3 = head3;
+   while(temp3){
+    printf("\n%d\n",temp3->data);
+    temp3 = temp3->next;
    }
  return 0;
 }
