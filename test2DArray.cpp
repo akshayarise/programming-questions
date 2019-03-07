da864e16e8f68cf5c08acfbd500e3c066929012f
akshayarise <akshayawakearise@gmail.com>
2019-03-07 23:23:59 +0530

    indented test2DArray is pushed

--- test2DArray.cpp
+++ test2DArray.cpp
@@ -7,7 +7,7 @@ int main()
     char arr[][50] = {"GeeksforGeeks", "Practice.GeeksforGeeks", "GeeksQuiz"};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    cout << strcmp("GeeksforG", "GeeksQuiz")<<"\n";
+    cout << strcmp("GeeksforG", "GeeksQuiz") << "\n";
 
     return 0;
 }
